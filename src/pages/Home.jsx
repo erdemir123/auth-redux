45451c9b06e1f2cf7ef97ae48aedbe17a4c9a713
erdemir123 <sadikerdemir93@gmail.com>
2022-11-17 01:28:40 +0300

    loading gif added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../conponents/Navbar";
 import { useFetch } from "../auth/functions";
 import Card from "../conponents/Card";
+import loading from "../asset/loading.gif";
 
 const Home = () => {
   const { isLoading, cardList } = useFetch();
@@ -9,9 +10,17 @@ const Home = () => {
   return (
     <div>
       <Navbar />
-      <div className="flex flex-wrap gap-6 justify-center items-center">
-      {cardList?.map((item,index)=><Card item={item} key={index}/>)}
-      </div>
+      {!isLoading ? (
+        <div className="flex flex-wrap gap-6 justify-center items-center">
+          {cardList?.map((item, index) => (
+            <Card item={item} key={index} />
+          ))}
+        </div>
+      ) : (
+        <div>
+          <img src={loading} alt="" className="mt-20"/>
+        </div>
+      )}
     </div>
   );
 };
